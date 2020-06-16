dfae0d953cffaf56b0e146dab7424f4f467c10c4
ItsMeVArun5 <varunmewada1@gmail.com>
2020-06-16 15:10:07 +0530

    solved

---
+++ problem_solving/extraLongFactorial.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+
+
+def extraLongFactorials(n):
+    if (n == 1):
+        return 1
+
+    return n * extraLongFactorials(n-1)
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    result = extraLongFactorials(n)
+
+    print(result)
